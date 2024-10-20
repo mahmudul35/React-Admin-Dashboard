483a620287dd8a9100e549a1b81572dd1fc59315
mahmudul35 <101060541+mahmudul35@users.noreply.github.com>
2024-10-20 13:27:20 +0600

    tailwind css and other dependency added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
-      <h1>Hello</h1>
+      <Routes>
+        <Route path="/" element={<Header />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "../components/header.css";
+const Header = () => {
+  return <div className="text-3xl">Headerr</div>;
+};
+
+export default Header;
